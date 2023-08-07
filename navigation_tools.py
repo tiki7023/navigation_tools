8efcf08bc00448e9a41a0a6d90f3e93ee422f7fa
tikill <35892546+tikill@users.noreply.github.com>
2023-08-07 20:51:16 +0800

    main

--- navigation_tools.py
+++ navigation_tools.py
@@ -1,1 +1,6 @@
-
+with open("file.txt", "r",encoding='gb18030',errors='ignore') as f:
+    lines = f.readlines()
+    words = []
+    for line in lines:
+        words.append(line.strip().split())
+    print(words)
