e30b35f9353796bbdb20dfb550fcb14839e1069f
tikill <35892546+tikill@users.noreply.github.com>
2023-08-07 22:30:48 +0800

    main

--- navigation_tools.py
+++ navigation_tools.py
@@ -1,6 +1,29 @@
-with open("file.txt", "r",encoding='gb18030',errors='ignore') as f:
-    lines = f.readlines()
-    words = []
-    for line in lines:
-        words.append(line.strip().split())
-    print(words)
+import re
+
+with open("file.html", "r",encoding='utf-8') as f:
+    content = f.read()
+############
+# pattern = re.compile(r"<strong>(.*?)</strong>")
+# match = pattern.search(content)
+# if match:
+#     title = match.group(1)
+#
+# options = [["Option 1", "Option 2", "Option 3"], ["Option A", "Option B", "Option C"]]
+# selected_option = options[0][0]
+#
+# content = re.sub(r"<strong>.*?</strong>", f"<strong>{selected_option}</strong>", content)
+
+###########
+pattern = re.compile(r"'(.*?)', '_blank'")
+match = pattern.search(content)
+if match:
+    title = match.group(1)
+
+options = [["Option 1", "Option 2", "Option 3"], ["Option A", "Option B", "Option C"]]
+selected_option = options[0][0]
+
+content = re.sub(r"'.*?', '_blank'", f"'{selected_option}', '_blank'", content)
+
+content = content+'\n'+content
+
+print(content)
