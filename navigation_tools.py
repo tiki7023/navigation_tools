06f5cacdabd04279468b9f26835b8acc6869dedc
tikill <35892546+tikill@users.noreply.github.com>
2023-08-08 13:33:23 +0800

    main

--- navigation_tools.py
+++ navigation_tools.py
@@ -1,29 +1,34 @@
 import re
 
+
+with open('option.txt', 'r',encoding='utf-8') as f:
+    lines = f.readlines()
+    options = []
+    for line in lines:
+        options.append(line.strip().split())
+
 with open("file.html", "r",encoding='utf-8') as f:
     content = f.read()
-############
-# pattern = re.compile(r"<strong>(.*?)</strong>")
-# match = pattern.search(content)
-# if match:
-#     title = match.group(1)
-#
-# options = [["Option 1", "Option 2", "Option 3"], ["Option A", "Option B", "Option C"]]
-# selected_option = options[0][0]
-#
-# content = re.sub(r"<strong>.*?</strong>", f"<strong>{selected_option}</strong>", content)
 
-###########
-pattern = re.compile(r"'(.*?)', '_blank'")
-match = pattern.search(content)
-if match:
-    title = match.group(1)
+html = ''
 
-options = [["Option 1", "Option 2", "Option 3"], ["Option A", "Option B", "Option C"]]
-selected_option = options[0][0]
+for i in range(len(options)):
+    selected_option = options[i][0]
+    content = re.sub(r"'.*?', '_blank'", f"'{selected_option}', '_blank'", content)
 
-content = re.sub(r"'.*?', '_blank'", f"'{selected_option}', '_blank'", content)
+    selected_option = options[i][0]
+    content = re.sub(r'le=".*?">', f'le="{selected_option}">', content)
 
-content = content+'\n'+content
 
-print(content)
+    selected_option = options[i][1]
+    content = re.sub(r'src=".*?" class="', f'src="{selected_option}" class="', content)
+
+    selected_option = options[i][2]
+    content = re.sub(r"<strong>.*?</strong>", f"<strong>{selected_option}</strong>", content)
+
+    selected_option = options[i][3]
+    content = re.sub(r'overflowClip_2">.*?</p>', f'overflowClip_2">{selected_option}</p>', content)
+
+    html = html+content
+
+print(html)
