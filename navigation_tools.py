35660ad681e65b6bf226548bfb7a06363aa93cc8
tikill <35892546+tikill@users.noreply.github.com>
2023-08-10 10:25:40 +0800

    main

--- navigation_tools.py
+++ navigation_tools.py
@@ -1,22 +1,24 @@
 import re
 import pandas as pd
+# 分类表
 sheets = ['常用网站','办公工具','视频网站','资源搜索','学校网站','学术网站','游戏相关','程序员','社交网站','数码跑分','导航页']
 result_total = ''
+# 读取网站信息并分类保存到txt文本中
 for i in range(len(sheets)):
-    df = pd.read_excel('option.xlsx',sheet_name = sheets[i])
+    df = pd.read_excel('option.xlsx',sheet_name = sheets[i],header=None)
     df.to_csv('.\website\{name}.txt'.format(name = sheets[i]), header=None, sep=' ', index=False)
-
+# 读取分类的txt网站信息，并加入列表中
     with open('.\website\{name}.txt'.format(name = sheets[i]), 'r',encoding='utf-8') as f:
         lines = f.readlines()
         options = []
         for line in lines:
             options.append(line.strip().split())
-        print(options)
-
-    with open("file.html", "r", encoding='utf-8') as f:
+        # print(options)
+# 读取网站模板中段
+    with open("website_mode_1.html", "r", encoding='utf-8') as f:
         content = f.read()
     html = ''
-
+# 将网站信息填入到模板中
     for j in range(len(options)):
         selected_option = options[j][0]
         content = re.sub(r"'.*?', '_blank'", f"'{selected_option}', '_blank'", content)
@@ -37,16 +39,19 @@ for i in range(len(sheets)):
             html = content
         else:
             html = html + '\n' +content
+# 加入分类头
     html = '\t\t\t'+'<h4 class="text-gray"><i class="linecons-tag" style="margin-right: 7px;" id="'+sheets[i]+'"></i>'+sheets[i]+'</h4>'+'\n'+'\t\t\t'+'<div class="row">'+'\n'+html
 
     result_total =result_total+'\n'+html+'\n'+'</div>'
-    print(html)
-
-    with open(r'.\results\{name}.txt'.format(name = sheets[i]), 'w',encoding='utf-8') as f:
+    # print(result_total)
+# 存到txt文件下
+    with open(r'.\data\{name}.txt'.format(name = sheets[i]), 'w',encoding='utf-8') as f:
         f.write(html)
-with open(r'head.html','r',encoding='utf-8') as f:
+
+# 生成完整html代码
+with open(r'website_model_2.html', 'r', encoding='utf-8') as f:
     head = f.read()
-with open(r'foot.html','r',encoding='utf-8') as f:
+with open(r'website_model_3.html', 'r', encoding='utf-8') as f:
     foot = f.read()
 result_total = head + result_total + foot
 with open(r'.\index.html', 'w',encoding='utf-8') as f:
